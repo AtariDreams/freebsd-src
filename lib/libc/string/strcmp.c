69b125e96342fe973a985bbb0eefe69746cecd4e
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-13 19:45:44 -0400

    Update strcmp.c

--- lib/libc/string/strcmp.c
+++ lib/libc/string/strcmp.c
@@ -49,7 +49,6 @@ strcmp(const char *s1, const char *s2)
 	while (*s1 == *s2) {
 		if (*s1 == '\0')
 			return (0);
-
 		s1++, s2++;
 	}
 	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
