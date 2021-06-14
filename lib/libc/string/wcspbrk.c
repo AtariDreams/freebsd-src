fc86ca9e97cc8c0e237d926279e8392af131af91
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-13 20:18:36 -0400

    Update wcspbrk.c

--- lib/libc/string/wcspbrk.c
+++ lib/libc/string/wcspbrk.c
@@ -44,17 +44,13 @@ wcspbrk(const wchar_t *s, const wchar_t *set)
 	const wchar_t *p;
 	const wchar_t *q;
 
-	p = s;
-	while (*p) {
-		q = set;
-		while (*q) {
+	for (p = s; *p; p++) {
+		for (q = set; *q; q++) {
 			if (*p == *q) {
 				/* LINTED interface specification */
 				return (wchar_t *)p;
 			}
-			q++;
 		}
-		p++;
 	}
 	return NULL;
 }
